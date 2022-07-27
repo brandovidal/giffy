bf67fdf94fbd21f24e3d6f5689b04afa1fceeaa0
Brando Vidal <yisusdracon@gmail.com>
2022-07-27 01:56:24 -0500

    ⚒️ Use wouter and links
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 
+import { Link, Route } from 'wouter'
+
 // Components
 import ListOfGifs from './components/ListOfGifs'
 
 export default function App () {
   return (
     <div className='App'>
-      <ListOfGifs keyword='panda' />
+      <h1>App</h1>
+
+      <Link to='/gif/panda'>Gifs de panda</Link>
+      <Link to='/gif/rick'>Gifs de Rick</Link>
+
+      <Route path='/gif/:keyword' component={ListOfGifs} />
     </div>
   )
 }
--- src/components/ListOfGifs.jsx
+++ src/components/ListOfGifs.jsx
@@ -6,17 +6,27 @@ import getGifs from '../services/getGifs'
 // Components
 import Gif from './Gif'
 
-export default function ListOfGifs ({ keyword } = {}) {
+export default function ListOfGifs ({ params }) {
+  const { keyword } = params
+
+  const [loading, setLoading] = useState(false)
   const [gifs, setGifs] = useState([])
 
   useEffect(() => {
-    getGifs({ keyword })
-      .then(gifs => setGifs(gifs))
+    setLoading(true)
+    getGifs({ keyword }).then((gifs) => {
+      setGifs(gifs)
+      setLoading(false)
+    })
   }, [keyword])
 
+  if (loading) return <i>Cargando</i>
+
   return (
-    gifs.map(({ id, title, url }) => (
-      <Gif key={id} id={id} title={title} url={url} />
-    ))
+    <div>
+      {gifs.map(({ id, title, url }) => (
+        <Gif key={id} id={id} title={title} url={url} />
+      ))}
+    </div>
   )
 }
